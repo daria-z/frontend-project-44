7be1a86bb973d06ba69125af981c39eed5e3dcbf
daria-z <daria-z>
2024-11-04 10:15:57 +0200

    fix logic fix for linter

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,4 +1,4 @@
 #!/usr/bin/env node
-import { greeting, evenChecking } from '../src/cli.js';
+import { evenChecking } from '../src/cli.js';
 
-evenChecking(greeting());
+evenChecking();
--- src/games/evenChecking.js
+++ src/games/evenChecking.js
@@ -1,18 +1,23 @@
 import readlineSync from 'readline-sync';
 
-export default (name) => {
+export default () => {
+  console.log('Welcome to the Brain Games!');
+
+  const name = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${name}!`);
   console.log('\'yes\' is wrong answer ;(. Correct answer was \'no\'.');
+
   for (let i = 1; i <= 3; i += 1) {
     const number = Math.floor(Math.random() * 50);
     console.log(`Question: ${number}`);
     const userAnswer = readlineSync.question('Your answer: ');
     const correctAnswer = number % 2 === 0 ? 'yes' : 'no';
-    if (userAnswer === correctAnswer) {
-      console.log('Correct!');
-    } else {
+    if (userAnswer !== correctAnswer) {
       console.log(`'${userAnswer}' is wrong answer ;( .Correct answer was '${correctAnswer}'.`);
-      return console.log(`Let's try again, ${name}!`);
+      console.log(`Let's try again, ${name}!`);
+      return;
     }
+    console.log('Correct!');
   }
-  return console.log(`Congratulations, ${name}!`);
+  console.log(`Congratulations, ${name}!`);
 };
